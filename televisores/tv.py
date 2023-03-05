011e58d5c9da41332e3a2c7c3d5661d341d07602
DiegoGG512 <124928460+DiegoGG512@users.noreply.github.com>
2023-03-04 23:14:36 -0500

    solución python

--- televisores/tv.py
+++ televisores/tv.py
@@ -46,6 +46,10 @@ class TV:
   @classmethod
   def getNumTV(cls):
     return cls._numTV
+  
+  @classmethod
+  def setNumTV(cls, numTV):
+    cls._numTV = numTV
 
   def turnOn(self):
     self._estado = True
